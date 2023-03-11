bb54124c8a8e71255f17f26ace99e50d090a0935
DragonNexa <tirtha070@gmail.com>
2023-03-11 21:49:15 +0530

    Made the app ready to be published

---
+++ android/app/src/main/kotlin/com/liquidgalaxy/lg_controller_2/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.liquidgalaxy.lg_controller_2
+
+import android.os.Bundle
+import android.content.Intent
+import android.util.Log
+import android.app.SearchManager
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+    val intent = getIntent()
+    val action = intent.getAction()
+
+    if (Intent.ACTION_SEARCH.equals(action)) {
+      val query = intent.getStringExtra(SearchManager.QUERY)
+      Log.e("Search received",query)
+    }
+  }
+}
